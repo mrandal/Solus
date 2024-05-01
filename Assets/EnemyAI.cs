a355f1159ec301508844504d26078d4722b81e05
mrandal <matrandal06@gmail.com>
2024-05-01 12:48:45 -0500

    enemy ai + jumpscare completed

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,31 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     #region Editor Data
     [Header("Movement Attributes")]
     [SerializeField] float _walkSpeed = 3f;
-    [SerializeField] float _sprintSpeed = 6f;
     [SerializeField] Rigidbody2D _rb;
-    public Image StaminaBar;
-    Animator animator;
-    public float Stamina, MaxStamina;
-    public float RunCost;
-    public float StaminaRegen;
-    private Coroutine recharge;
-
+    [SerializeField] Rigidbody2D damien;
+    [SerializeField] string menuScene;
+    //Animator animator;
     #endregion
 
     #region Internal Data
     private Vector2 _moveDir = Vector2.zero;
+    private Vector2 zeroPoint = Vector2.zero;
     #endregion
 
     void Start()
     {
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
     }
     #region Tick
     private void Update()
@@ -42,39 +38,20 @@ public class PlayerController : MonoBehaviour
     #region Input Logic
     private void GatherInput()
     {
-        _moveDir.x = Input.GetAxisRaw("Horizontal");
-        _moveDir.y = Input.GetAxisRaw("Vertical");
+        
+        _moveDir = damien.GetRelativePoint(zeroPoint)-_rb.GetRelativePoint(zeroPoint);
+        print(_moveDir);
     }
     #endregion
 
     private void MovementUpdate()
     {
         _moveDir.Normalize();
-        if(Input.GetKey(KeyCode.LeftShift) && Stamina > 0)
-        {
-            _rb.AddForce(_moveDir * _sprintSpeed * Time.fixedDeltaTime);
-            if(_moveDir != Vector2.zero)
-            {
-                Stamina -= RunCost * Time.fixedDeltaTime;
-                if(Stamina < 0)
-                {
-                    Stamina = 0;
-                }
-                StaminaBar.fillAmount = Stamina / MaxStamina;
+        _rb.AddForce(_moveDir * _walkSpeed * Time.fixedDeltaTime);
 
-                if(recharge != null)
-                {
-                    StopCoroutine(recharge);
-                }
-                recharge = StartCoroutine(RechargeStamina());
-            }
-        }
-        else
-        {
-            _rb.AddForce(_moveDir * _walkSpeed * Time.fixedDeltaTime);
-        }
         //animate
         // Directions: 0 = no movement, 1 = left, 2 = up, 3 = right, 4 = down
+        /*
         if(Mathf.Abs(_moveDir.x) > 0 && Mathf.Abs(_moveDir.x) >= Mathf.Abs(_moveDir.y))
         {
             if(_moveDir.x > 0)
@@ -101,18 +78,14 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetInteger("walkDirection",0);
         }
-        
+        */
     }
-    
-    private IEnumerator RechargeStamina()
-    {
-        yield return new WaitForSeconds(1f);
 
-        while(Stamina < MaxStamina)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject == damien)
         {
-            Stamina += StaminaRegen * Time.fixedDeltaTime;
-            StaminaBar.fillAmount = Stamina / MaxStamina;
-            yield return new WaitForFixedUpdate();
+            SceneManager.LoadScene(menuScene);
         }
     }
 }
